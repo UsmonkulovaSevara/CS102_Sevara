22bbde8aff602d6e1db05fea56b697f190a12d05
UsmonkulovaSevara <250130@newuu.uz>
2026-01-27 12:35:55 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+
+     cout << "Sevara"<<endl;
+     cout << "Pedagogy in STEM specialization\n";
+     cout <<"250130\n";
+     cout <<"27/01/2026";
+    return 0;
+}
