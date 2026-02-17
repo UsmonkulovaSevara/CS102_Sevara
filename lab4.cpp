cf8039dc3c6127b45e798e3c08754aecee7d1bd2
UsmonkulovaSevara <250130@newuu.uz>
2026-02-17 12:51:19 +0500

    first commit

---
+++ lab4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+int lab4() {
+
+
+
+
+return 0;
+}
--- main.cpp
+++ main.cpp
@@ -4,114 +4,70 @@
 using namespace std;
 
 int main() {
-     int intNumber = 15;
-     cout << intNumber << "\t" << sizeof(intNumber) << endl;
 
-     float floatNumber = 3.14;
-     cout << floatNumber << "\t" << sizeof(floatNumber) << endl;
+     cout << "Problem 2" << endl;
+     double x1, x2, x3;
+     cin >> x1 >> x2 >> x3;
+     double mu=(x1 + x2 + x3)/3.0;
+     double var = ((pow((x1-mu), 2) + (pow((x2-mu), 2)) + (pow((x3-mu), 2))))/3.0;
+     cout << var << endl;
 
-     double doubleNumber = 45.1234;
-     cout << doubleNumber <<"\t" <<sizeof(doubleNumber) << endl;
+     cout << "Problem 3" << endl;
+     double p;
+     cin >> p;
+     if (p<0)
+          cout << "The number is negative" << endl;
+     else if (p>0)
+          cout << "The number is positive" << endl;
+     if (p==0)
+          cout << "The number is zero" << endl;
 
-     bool boolean = true;
-     cout << boolean << "\t" <<sizeof(boolean) << endl;
+     cout << "Problem 4" << endl;
+     int q;
+     cin >> q;
+     if (q % 2==0) {
+          cout << "The number is even";
+     } else {cout << "The number is odd" ; } cout << endl;
 
-     char charName = 65;
-     cout << charName << "\t" << sizeof(charName) << endl;
+     cout << "Problem 5" << endl;
+     double t,h;
+     cin >> t >> h;
+     if (t>h)
+          cout << "Largest number is: " << t << endl;
+     else if (h>t)
+          cout << "Largest number is: " << h << endl;
+     else if (t==h)
+          cout << "The numbers are equal" << endl;
 
-     cout << " \"I am standing\" on the edge of some crazy cliff.\n"
-             "What I have to do, I have to cach everybody\\ \n"
-             "if they start to go over the cliff-I mean\\ \n"
-             "\\if they're running and they don't look \n"
-             "where they're going I have to come out \n"
-             "from somewhere and \t \t catch them. " <<endl;
+     cout << "Problem 6" << endl;
+     double a,b,c;
+     double largest;
+     cin >> a >> b >> c;
+     if (a>b)
+          if (a>c)
+               largest = a;
+     else { largest = c;}
+     else if (b>a)
+          if (b>c)
+               largest = b;
+     cout << "Largest number is: " << largest << endl;
 
-    int a=189;
-    char b='B';
-    float division=a/3;
-    cout << division << "\t";
-    float decimalvalue=b;
-    cout << decimalvalue << "\t";
-    float addition=(a+b)/5;
-    cout << addition << "\t";
-    char character=b+(a/10-1);
-    cout << character << endl;
+     cout << "Problem 7" << endl;
+     double speed;
+     cin >> speed;
+     if (speed<20 && speed>0)
+          cout << "Too slow";
+     else if (speed>80)
+          cout << "Too fast";
+     else cout << "Just right";
 
-    int value1;
-    int value2;
-    int sum;
-    cout << "Enter value 1: ";
-    cin >> value1;
-    cout << "Enter value 2: ";
-    cin >> value2;
-    sum = value1 + value2;
-    cout << "Sum is:" << sum << endl;
+     cout << "Problem 11" << endl;
+     char light;
 
-    int x;
-    int y;
-    cout << "Enter value 1: ";
-    cin >> x;
-    cout << "Enter value 2: ";
-    cin >> y;
-    int t=(3*((x*x)+3))/((y/4)+3);
-    cout << "t=" << t << endl;
 
-    int d;
-    cout << "Enter diameter value: ";
-    cin >> d;
-    float p=3.14;
-    int r=d/2;
-    float area = 4*p*r*r;
-    cout << "Area of the sphere:" << area << endl;
 
-    float v1;
-    float v0;
-    float time;
-    cout << "Enter starting velocity (in meters per second): ";
-    cin >> v0;
-    cout << "Enter ending velocity (in meters per second): ";
-    cin >> v1;
-    cout << "Enter time span (in seconds) : ";
-    cin >> time;
-    float av=(v1-v0)/time;
-    cout << "Average velocity: " << av << endl;
 
-    float distance;
-    float fuel;
-    float price;
-    cout << "Enter distance in miles: ";
-    cin >> distance;
-    cout << "Enter fuel in miles per gallon: ";
-    cin >> fuel;
-    cout << "Enter price per gallon: ";
-    cin >> price;
-    float cost=(distance/fuel)*price;
-    cout << "The cost of driving is: $" << cost << endl;
 
-    double f=1.13e3;
-    double g=411e-4;
-    cout << f << "\t" << g << endl;
-
-    float k;
-    cout << "Enter a number: ";
-    cin >> k;
-    int l=k;
-    double fraction=k-l;
-    cout << "Fractional part: " << fraction << endl;
-
-    int students;
-    cout << "Enter number of students: ";
-    cin >> students;
-    int first = students/11;
-    cout << "first group:" << first << endl;
-    int last = students % 11;
-    cout << "last group:" << last << endl;
-
-    float s;
-    cout << "Enter the side of the hexagon:";
-    cin >> s;
-    float hexagon=((3*sqrt (3))/2)*s*s;
-    cout << "Area of the hexagon:" << hexagon << endl;
 
      return 0;
 }
