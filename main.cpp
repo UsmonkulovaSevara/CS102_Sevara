9aa142428e5aa545dd52199fd765684569e59591
UsmonkulovaSevara <250130@newuu.uz>
2026-03-05 11:00:02 +0500

    first commit

--- main.cpp
+++ main.cpp
@@ -2,98 +2,110 @@
 #include <iomanip>
 #include <math.h>
 using namespace std;
-
-int main() {
-    cout << "Problem 7" << endl;
-    int num;
-    bool isprime=true;
-    cout << "Enter a number: ";
-    cin >> num;
-    if (num<=1) {
-        isprime=false;
-    } else {
-        for (int i=2;i<num;i++) {
-            if (num%i==0) {
-                isprime=false;
-                break;
-            }
-        }
+//6.6
+long long Factorial(int n=1) {
+    long long result = 1;
+    for (int i=1; i<= n; i++) {
+        result= result*i;
     }
-    if (isprime)
-        cout << "Number is prime" << endl;
-    else
-        cout << "Number is not prime" << "\n";
+    return result;
+}
+int main1() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Factorial of " << n << " is " << Factorial(n) << endl;
+    return 0;
+}
+//6.7
+int gcd(int num1, int num2) {
+    while (num2 !=0) {
+        int r = num2;
+        num2 = num1 % num2;
+        num1 = r;
+    }
+    return num1;
+}
+int main2() {
+    int num1, num2;
+    cout << "Enter numbers: ";
+    cin >> num1 >> num2;
+    cout << "GCD of these numbers is " << gcd(num1, num2) << endl;
+    return 0;
+}
+//6.8
+void Maximum(int a, int b, int c) {
+    int max = a;
 
-    cout << "Problem 8" << endl;
-    int n, temp, c = 0;
-    int R = 0;
+    if (b>max)
+        max = b;
+    if (c>max)
+        max=c;
+    cout << "Maximum: " << max;
+}
+void Minimum(int a, int b, int c) {
+    int min=a;
 
+    if (min>b)
+        min=b;
+    if (min>c)
+        min=c;
+    cout << "Minimum: " << min;
+}
+int main3() {
+    int a,b,c;
+    cout << "Enter the numbers: ";
+    cin >> a >> b >> c;
+    Maximum (a,b,c);
+    cout << endl;
+    Minimum (a,b,c);
+    return 0;
+}
+//6.9
+int sum (int a, int b) {
+    return a+b;
+}
+int subtract (int a, int b) {
+    return a-b;
+}
+int product (int a, int b) {
+    return a*b;
+}
+int division (int a, int b) {
+    return a/b;
+}
+int main4() {
+    int a, b,c;
+    cout << "Enter 2 numbers: ";
+    cin >> a >> b;
+    cout << sum(a,b) << endl << subtract(a,b) << endl << product(a,b) << endl << division << endl;
+    return 0;
+}
+//6.10
+int getTriangularNumber (int n) {
+    return n*(n+1)/2;
+}
+int main5() {
+    for (int i=1;i<=75;i++) {
+        cout << getTriangularNumber (i) << " ";
+        if ( i % 5 == 0)
+        cout << endl;
+    }
+    return 0;
+}
+//6.11
+void displayeven (int n) {
+    while (n>0) {
+        int d=n%10;
+        n=n/10;
+        if (d % 2==0)
+            cout << d << endl;
+    }
+}
+int main() {
+    int n;
     cout << "Enter number: ";
     cin >> n;
-
-    temp = n;
-
-    while (temp > 0) {
-        temp = temp / 10;
-        c++;
-        }
-
-    int k = c - 1;
-
-    while (n > 0) {
-        int d = n % 10;
-        n = n / 10;
-        R = R + d * pow(10, k);
-        k--;
-        }
-
-    cout << R << endl;
-
-    cout << "Problem 9" << endl;
-    int number;
-    int sum=0;
-    cout << "Enter a nuber: ";
-    cin>>n;
-
-    for (int i=1; i<=n; i++) {
-        int square = i*i;
-        cout << i << "*" << i << "=" << square << endl;
-        sum += square;
-    }
-    cout << "Sum=" << sum << endl;
-
-
-    cout << "Problem 10" << endl;
-    int max=0;
-    int numb;
-    do {
-        cout << "Enter a number: ";
-        cin >> numb;
-        if (max<numb)
-            max=numb;
-    }
-        while (numb!=0);
-    cout << "Maximum number is: " << max << endl;
-
-    cout << "Problem 11" << endl;
-    int v;
-    int positive=0;
-    int negative=0;
-    int total=0;
-    float average;
-    do {
-        cout << "Enter values: ";
-        cin >> v;
-        if (v>0)
-            positive=positive+1;
-        else if (v<0)
-            negative=negative+1;
-        total=total+v;
-    }
-    while (v!=0);
-    average=float(total/(positive+negative));
-    cout << "The number of positives is " << positive << "\n" << "The number of negatives is " << negative << "\n"
-    << "The total is " << total << "\n" << "The average is " << average << endl;
-
+    displayeven(n);
     return 0;
 }
